4e8969dbc1e6555a3c8c2250e8f487d6faa1309b
caseylister <caseynlister@gmail.com>
2021-12-07 18:34:51 -0500

    update sess details

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-
 const helpers = require('./utils/helpers');
+
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
@@ -16,8 +16,9 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'bigbluedog',
+  secret: 'supersecret',
   cookie: {
+    // Session expires after 10 minutes
         expires: 10 * 60 * 1000
   },
   resave: true,
