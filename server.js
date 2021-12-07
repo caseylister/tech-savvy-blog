cf80df5d1621fe59e96f3e47367ccf5347763eda
caseylister <caseynlister@gmail.com>
2021-12-07 18:27:54 -0500

    update structure

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+const helpers = require('./utils/helpers');
+
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
 const session = require('express-session');
 
 const app = express();
@@ -12,19 +16,19 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'codingrules',
-    cookie: {},
-    resave: true,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-        checkExpirationInterval: 1000 * 60 * 10,
-        expiration: 1000 * 60 * 30
-    })
+  secret: 'bigbluedog',
+  cookie: {
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
 };
 
 app.use(session(sess));
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
